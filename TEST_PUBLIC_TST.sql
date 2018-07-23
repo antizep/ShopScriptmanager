0f797ade90ded572521ffe15f35799da0a982b88
antiz <antizep@mail.ru>
2018-07-23 10:26:36 +0300

    in the form of the creation of the goods added the article

---
+++ TEST_PUBLIC_TST.sql
@@ -0,0 +1,3 @@
+insert into PUBLIC.ROLES (ID_ROLE, CHMOD, NAME) values (1, 1, 'adm');
+insert into PUBLIC.PROVIDER (ID, MIN_PAY, NAME, REMARK, URL) values (1, 10000, 'ashot', 'dwdawd', 'http://ashot.ru');
+insert into PUBLIC.AUTHENTICATION (ID, LOGIN, PASSWORD, ROLE) values (1, 'az', 1414, 1);
